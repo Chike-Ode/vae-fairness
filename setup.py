1e1169cc00e3e9b0853ea4df3dc6443e13843fea
Chike-Ode <chikeodenigbo@gmail.com>
2023-02-25 18:54:30 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Survey article on the variational autoencoders for fairness.',
+    author='Chike Odenigbo, Karthika Kumar, Mina Arzaghi (HEC Montreal)',
+    license='MIT',
+)
